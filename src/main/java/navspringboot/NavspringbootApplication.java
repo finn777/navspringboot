71f0a8e1c865f2e107394fdcaa0b93298cfc300c
Alexey Finogenov <finn777@yandex.ru>
2016-09-28 17:09:06 +0300

    First
    navobjectpermissionsreportwebapp via Spring Boot

---
+++ src/main/java/navspringboot/NavspringbootApplication.java
@@ -0,0 +1,12 @@
+package navspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavspringbootApplication.class, args);
+	}
+}
